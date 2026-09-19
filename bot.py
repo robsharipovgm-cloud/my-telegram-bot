9b0e1019b2bafa15b334781139358e44fb3f6f31
robsharipovgm-cloud <robsharipov.gm@gmail.com>
2026-09-19 14:58:02 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,8 @@
-import os
 import telebot
 import random
+import os
+import threading
+from http.server import BaseHTTPRequestHandler, HTTPServer
 
 bot = telebot.TeleBot(os.environ.get('BOT_TOKEN'))
 
@@ -11,5 +13,21 @@ def yes_no(message):
 @bot.message_handler(func=lambda message: True)
 def echo(message):
     bot.reply_to(message, "Напиши /yesno")
-bot.remove_webhook()
-bot.infinity_polling()
+
+# Это маленький сайт, чтобы Render видел, что бот работает
+class WebServer(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b"Bot is running")
+
+def run_web_server():
+    port = int(os.environ.get('PORT', 8080))
+    server = HTTPServer(('0.0.0.0
+', port), WebServer)
+    server.serve_forever()
+
+if __name__ == "__main__":
+    bot.remove_webhook()
+    threading.Thread(target=run_web_server).start()
+    bot.infinity_polling()
