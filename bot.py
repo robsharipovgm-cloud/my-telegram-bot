ab8fdd99f5ea5490d0a2d5404f3c1408a4ba168d
robsharipovgm-cloud <robsharipov.gm@gmail.com>
2026-09-19 21:40:27 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -21,15 +21,12 @@ class WebServer(BaseHTTPRequestHandler):
         self.wfile.write(b"Bot is running")
 
 def run_web_server():
-    port = int(os.environ.get('PORT', 8080))    
-        server = HTTPServer(('0.0.0.0',
-                             port), WebServer)
+    port = int(os.environ.get('PORT', 8080))
+    server = HTTPServer(('0.0.0.0
+', port), WebServer)
     server.serve_forever()
 
 if __name__ == "__main__":
     bot.remove_webhook()
     threading.Thread(target=run_web_server).start()
     bot.infinity_polling()
-
-
-
