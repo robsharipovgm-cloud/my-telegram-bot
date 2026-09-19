58887d06b7f257c2e358520fdd23dc52f81f27a5
robsharipovgm-cloud <robsharipov.gm@gmail.com>
2026-09-20 00:26:02 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,40 +2,40 @@ import telebot
 import random
 import os
 import threading
-from http.server import (
-    BaseHTTPRequestHandler,
-    HTTPServer,
-)
+from http.server import BaseHTTPRequestHandler
+from http.server import HTTPServer
 
-bot = telebot.TeleBot(
-    os.environ.get('BOT_TOKEN')
-)
+bot_token = os.environ.get('BOT_TOKEN')
+bot = telebot.TeleBot(bot_token)
 
 @bot.message_handler(commands=['yesno'])
 def yes_no(message):
-    answer = random.choice(["Да", "Нет"])
+    answer = random.choice(['Да', 'Нет'])
     bot.reply_to(message, answer)
 
 @bot.message_handler(func=lambda message: True)
 def echo(message):
-    bot.reply_to(message, "Напиши /yesno")
+    hint = 'Напиши /yesno'
+    bot.reply_to(message, hint)
 
 class WebServer(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
         self.end_headers()
-        msg = b"Bot is running"
-        self.wfile.write(msg)
+        text = 'Bot is running'
+        data = text.encode()
+        self.wfile.write(data)
 
 def run_web_server():
-    port_str = os.environ.get('PORT', 8080)
-    port = int(port_str)
-    addr = ('0.0.0.0
-', port)
+    port_text = os.environ.get('PORT', '8080')
+    port = int(port_text)
+    host = '0.0.0.0
+'
+    addr = host, port
     server = HTTPServer(addr, WebServer)
     server.serve_forever()
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     bot.remove_webhook()
     t = threading.Thread(target=run_web_server)
     t.start()
