14a09c7b8b2e68a65c2bbeabd22f2cc9cee0b616
robsharipovgm-cloud <robsharipov.gm@gmail.com>
2026-09-19 21:32:03 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -23,7 +23,7 @@ class WebServer(BaseHTTPRequestHandler):
 def run_web_server():
     port = int(os.environ.get('PORT', 8080))
     server = HTTPServer(('0.0.0.0
-', port), WebServer))
+', port), WebServer)
     server.serve_forever()
 
 if __name__ == "__main__":
