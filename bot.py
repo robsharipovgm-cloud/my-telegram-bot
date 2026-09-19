2313b250c1b1a9e6b7ba3be2332badbc45251de1
robsharipovgm-cloud <robsharipov.gm@gmail.com>
2026-09-19 22:29:59 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ import threading
 from 'http.server'
 import BaseHTTPRequestHandler, HTTPServer bot ='telebot'.TeleBot(os.environ.get ('BOT_TOKEN')) @bot.message_handler(commands= ['yesno'])
 def yes_no(message): bot.reply_to (message, random.choice(["Да", "Нет"]))@bot.message_handler(func = lambda message: True)
-def echo (message):bot.reply_to(message,'"Напиши /yesno")
+def echo (message):bot.reply_to(message,"Напиши /yesno")
 class WebServer(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
