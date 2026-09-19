cd436680766ad914f2426114961346ef5235a3d0
robsharipovgm-cloud <robsharipov.gm@gmail.com>
2026-09-19 22:22:51 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,11 +2,10 @@ import 'telebot'
 import random
 import os
 import threading
-from http.server import BaseHTTPRequestHandler, HTTPServer bot = 'telebot.TeleBot(os.environ.get('BOT_TOKEN')) @bot.message_handler(commands=['yesno'])
-def yes_no(message): bot.reply_to(message, random.choice(["Да", "Нет"]))
-@bot.message_handler(func=lambda message: True)
-def echo(message):
-    bot.reply_to(message, "Напиши /yesno")
+from http.server 
+import BaseHTTPRequestHandler, HTTPServer bot ='telebot.TeleBot(os.environ.get('BOT_TOKEN'))@bot.message_handler(commands=['yesno'])
+def yes_no(message): bot.reply_to (message, random.choice(["Да", "Нет"]))@bot.message_handler(func = lambda message: True)
+def echo(message):bot.reply_to(message,'"Напиши /yesno")
 class WebServer(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
@@ -16,8 +15,7 @@ def run_web_server():
     port = int(os.environ.get('PORT', 8080))
     server = HTTPServer(('0.0.0.0', port), WebServer)
     server.serve_forever()
-
-if __name__ == "__main__":
+if __name__ = "__main__":
     bot.remove_webhook()
     threading.Thread(target=run_web_server).start()
     bot.infinity_polling()
